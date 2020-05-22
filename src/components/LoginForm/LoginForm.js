56cb05f493c18d73c7264767662dea4f6e6bca1b
unknown <danieleidenilsen@hotmail.com>
2020-05-22 14:50:19 +0200

    update

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,17 +3,21 @@ import { useForm } from "react-hook-form"
 import * as yup from "yup";
 
 
-
+//This code are from module 3.4 but modified to this assigment.
 const schema = yup.object().shape({
+
     username: yup.string().required("Username is required"),
     password: yup
     .string()
     .required("Password is required")
     .min(4)
+
     
 });
 
 
+
+//Some of these codes are from module 3.4 but modified to this assigment.
 
 function LoginForm () {
     const { register, handleSubmit, errors } = useForm({
